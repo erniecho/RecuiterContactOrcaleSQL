23d38d99b48e0421f4a56e7934e9f93b4192c4a1
Ernie <choernie@yahoo.com>
2015-08-05 00:38:16 -0400

    I'm going take a break and work on it tomorrow.
    Starting from line 41. I try looking for a reason why it's not working.
    CM_ID has some type of problem with it.

--- CONTACT_DATEBASE.sql
+++ CONTACT_DATEBASE.sql
@@ -3,12 +3,17 @@ this code was writen by Ernie on 8/3/15
 Basically keeping track of the recuiters and keep myself from building a
 mess of folder fill with notes and numbers on my desk.
 */
+Drop Table Atest_Company;
+Drop Table Atest_Employee;
+Drop Table Atest_Log_SignIn;
+Drop Table Atest_Calls_Message;
+
 CREATE TABLE ATest_Company
 (
 Comp_ID VARCHAR(4000 byte) NOT NULL,
-Comp_Name VARCHAR(120 CHAR)NOT NULL,
-Comp_WEBD VARCHAR(200 CHAR)NOT NULL,
-PRIMARY KEY (Comp_ID)
+Comp_Name VARCHAR(120 CHAR),
+Comp_WEBD VARCHAR(200 CHAR),
+CONSTRAINT Comp_ID_pk PRIMARY KEY (Comp_ID)
 );
 
 CREATE TABLE Atest_Employee
@@ -21,7 +26,7 @@ Phone_Ext NUMBER(4)	NOT NULL,
 Phone_Cell NUMBER(11) NOT NULL,
 Emp_Postion_Title VARCHAR(30 CHAR) NOT NULL,
 Emp_Email VARCHAR(100 CHAR) NOT NULL,
-PRIMARY KEY (Employee_ID)
+CONSTRAINT Employee_ID_pk PRIMARY KEY (Employee_ID)
 );
 
 CREATE TABLE Atest_Log_SignIn
@@ -36,10 +41,10 @@ PRIMARY KEY (Log_ID)
 CREATE TABLE Atest_Calls_Message
 (
 CM_ID VARCHAR(4000 byte) NOT NULL,
-Message VARCHAR(400 byte) NOT NULL,
+Message VARCHAR(400 byte),
 Date_Time_Rec DATE,
-PRIMARY KEY (CM_ID),
-FOREIGN KEY (Comp_ID) REFERENCES Atest_Company(Comp_ID),
-FOREIGN KEY (Employee_ID) REFERENCES Atest_Employee (Employee_ID)
-FOREIGN KEY (Log_ID) REFERENCES Atest_Log_SignIn (Log_ID)
+CONSTRAINT CM_ID_pk PRIMARY KEY (CM_ID),
+CONSTRAINT fk_Comp_ID FOREIGN KEY Comp_ID) REFERENCES Atest_Company(Comp_ID),
+CONSTRAINT fk_Employee_ID FOREIGN KEY (Employee_ID) REFERENCES Atest_Employee (Employee_ID),
+CONSTRAINT fk_Log_ID FOREIGN KEY (Log_ID) REFERENCES Atest_Log_SignIn (Log_ID)
 );
